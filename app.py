d2eb4607bca1176d23251f128eeac65b013c1fbe
vunk.lai <vunk.lai@zyxel.com.tw>
2022-05-04 10:37:32 +0800

    feat(app): access the account and region passed from the AWS CLI in the stack

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import os
+
 import aws_cdk as cdk
 
 app = cdk.App()
-env = cdk.Environment(account='123', region='foo')
+env = cdk.Environment(
+    account=os.environ['CDK_DEFAULT_ACCOUNT'],
+    region=os.environ["CDK_DEFAULT_REGION"])
 
 stack = cdk.Stack(app, 'stack', env=env)
 
